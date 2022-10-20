ba731b3576c36670d2540f93432519367e9f0b72
unknown <rafalbiel77@gmail.com>
2022-10-20 20:42:40 +0200

    added prototype topics

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const newUser = {
 		brand: "VW",
 		color: "black",
 		year: 2019,
-        sound (){
-            console.log('brum brum');
-        }
+		sound() {
+			console.log("brum brum");
+		},
 	},
 };
 console.log(newUser.age);
@@ -18,24 +18,38 @@ console.log(newUser.car.color);
 console.log(
 	`${newUser.name} jeździ $ autem marki ${newUser.car.brand} w kolorze ${newUser.car.color}`
 );
-newUser.car.sound()
+newUser.car.sound();
 
-newUser.country = 'Polska';
-newUser['fav-movie']='Matrix';
+newUser.country = "Polska";
+newUser["fav-movie"] = "Matrix";
 
-
-const dogName = 'Rina';
+const dogName = "Rina";
 const age = 4;
-const dog={
-    name: dogName,
-    age: age
+const dog = {
+	name: dogName,
+	age: age,
 };
 delete dog.age;
 
-
 const user = {
-    name: 'Maja',
-    test(){
-        console.log(name);
-    }
+	name: "Maja",
+	test() {
+		console.log(name);
+	},
+};
+
+class User1 {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
 }
+
+User1.prototype.hello = function () {
+	console.log(`użytkownik ma na imię ${this.name} i ma ${this.age} lat`);
+};
+
+const newUser1 = new User1("Jola", 46);
+const newUser2 = new User1("Antek", 15);
+newUser1.hello();
+newUser2.hello();
